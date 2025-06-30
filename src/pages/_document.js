c078de930aa0576fb2d21c14e00b4f406a6288a8
mahnoor faisal <faisalmahnoor86@gmail.com>
2025-07-01 00:45:02 +0500

    tab bar title updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>My Portfolio</title>
+        <link rel="icon" type="image/png" href="/tab-icon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
